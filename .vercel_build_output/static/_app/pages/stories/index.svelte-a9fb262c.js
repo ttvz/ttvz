5aa876c397449498d9ff929961a0816365a25680
ttvz <thibaut.valez@gmail.com>
2021-10-24 19:30:19 +0200

    add vercel adapter

---
+++ .vercel_build_output/static/_app/pages/stories/index.svelte-a9fb262c.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e,T as a,d as o,I as i}from"../../chunks/vendor-1c69811a.js";function r(s){return{c(){this.h()},l(s){a('[data-svelte="svelte-iw4wjv"]',document.head).forEach(o),this.h()},h(){document.title="Thibaut Valez | Stories"},m:i,p:i,i:i,o:i,d:i}}class c extends s{constructor(s){super(),t(this,s,null,r,e,{})}}export{c as default};
