b3f600121851000a5b958fb3f681d7eb2a5d86e5
ttvz <thibaut.valez@gmail.com>
2024-06-25 15:43:29 +0200

    lighthouse - use vsharp to optimize images

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import vsharp from 'vite-plugin-vsharp';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		vsharp({
+			// Plugin options go here
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
