3c579d41527b028f93017e22b5c890f67a116f2d
ttvz <thibaut.valez@gmail.com>
2024-07-17 19:36:10 +0200

    update lang + deactivate vsharp

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import vsharp from 'vite-plugin-vsharp';
 
 export default defineConfig({
 	plugins: [
-		vsharp({
-		}),
 		sveltekit()
 	],
 	test: {
